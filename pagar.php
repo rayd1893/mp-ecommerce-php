76177f0f97e0d5b5c7e7e1a3b5362a85d0b29edf
rayd1893 <rayd1893@gmail.com>
2021-02-12 01:31:06 -0500

    Update pagar.php

--- pagar.php
+++ pagar.php
@@ -1,7 +1,7 @@
 
 <?php
 // SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+require __DIR__ .  'vendor/autoload.php';
 
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a
