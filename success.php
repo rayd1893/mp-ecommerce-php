d3afe51d3d75e579ca4f11b887c1475df451172b
rayd1893 <rayd1893@gmail.com>
2021-02-15 16:49:07 -0500

    Update success.php

--- success.php
+++ success.php
@@ -17,3 +17,12 @@
 </body>
 </html>
 
+<?php
+	require __DIR__ .  '/vendor/autoload.php';
+
+	$mp = new MP ("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
+
+	$payment = $mp->get(
+		"/v1/payments/". $_GET['payment_id']
+	);
+?>
