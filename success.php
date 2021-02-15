0cd72c0b0bb69c782b8c3b407d3f3f67465aa333
rayd1893 <rayd1893@gmail.com>
2021-02-15 16:55:49 -0500

    Update success.php

--- success.php
+++ success.php
@@ -23,6 +23,6 @@
 	$mp = new MP ("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
 
 	$payment = $mp->get(
-		"/v1/payments/". $_GET['payment_id']
+		"https://api.mercadopago.com/v1/payments/". $_GET['payment_id']
 	);
 ?>
