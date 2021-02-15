710f8d41efffca09d0e5499e249ffd910bcb0913
rayd1893 <rayd1893@gmail.com>
2021-02-15 14:28:34 -0500

    d

--- success.php
+++ success.php
@@ -17,28 +17,3 @@
 </body>
 </html>
 
-<?php
-require __DIR__ .  '/vendor/autoload.php';
-
-/*MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004"); */
-MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
-
-    /*switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
-    }*/
-
-    $payment = MercadoPago\Payment.find_by_id($_GET['payment_id']);
-
-    echo  $payment;
-?>
