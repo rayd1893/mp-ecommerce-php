00b2b1c559fcc6688dcf9cfa1aca96e89e2a6adc
rayd1893 <rayd1893@gmail.com>
2021-02-15 17:03:56 -0500

    Update success.php

--- success.php
+++ success.php
@@ -18,11 +18,12 @@
 </html>
 
 <?php
-	require __DIR__ .  '/vendor/autoload.php';
 
-	$mp = new MP ("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
+    $uri = 'https://api.mercadopago.com/v1/payments/';
+    $id_pago = $_GET['payment_id'];
+    $access = 'APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439';
 
-	$payment = $mp->get(
-		"https://api.mercadopago.com/v1/payments/". $_GET['payment_id']
-	);
+    $url = $uri.$id_pago.'?access_token='.$access;
+
+	echo file_get_contents($url);
 ?>
