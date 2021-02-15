891af7c40e2f02b2b191bdc8efc6115035fd986c
rayd1893 <rayd1893@gmail.com>
2021-02-15 13:53:54 -0500

    Update success.php

--- success.php
+++ success.php
@@ -19,7 +19,7 @@
 
 <?php
 
-/*MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004"); */
+MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004"); 
 MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
 
 
