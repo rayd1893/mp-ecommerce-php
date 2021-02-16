0ba1effc190de2821852637c4a3702f36539b629
rayd1893 <rayd1893@gmail.com>
2021-02-15 23:44:44 -0500

    Update andra_notificaciones.php

--- andra_notificaciones.php
+++ andra_notificaciones.php
@@ -1,11 +1,11 @@
 <?php
 
 $uri = 'https://api.mercadopago.com/v1/payments/';
-    $id_pago = $_GET['id'];
-    $access = 'APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439';
+$id_pago = $_POST['id'];
+$access = 'APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439';
 
     $url = $uri.$id_pago.'?access_token='.$access;
-
+	print $url;
 	echo file_get_contents($url);
 }
 
