b17485ca3d0b023a216b03e67655e11edcedea0e
rayd1893 <rayd1893@gmail.com>
2021-02-16 17:00:07 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -194,7 +194,7 @@ $preference->payment_methods = array(
     "pending" => "https://rayd1893-mp-commerce-php.herokuapp.com/pending.php"
 );
 $preference->auto_return = "approved";
-$preference->notification_url = "https://rayd1893-mp-commerce-php.herokuapp.com/notitas.php?source_news=webhooks";
+$preference->notification_url = "https://rayd1893-mp-commerce-php.herokuapp.com/notitas.php";
 
   $preference->external_reference = "rayd1893@gmail.com";
 
