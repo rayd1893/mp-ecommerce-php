11edfc29e072ce68503e0f05fd5dab2e728a49f5
rayd1893 <rayd1893@gmail.com>
2021-02-15 20:02:41 -0500

    Update andra_notificaciones.php

--- andra_notificaciones.php
+++ andra_notificaciones.php
@@ -1,5 +1,10 @@
 <?php
 
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Agrega credenciales
+MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004");
 MercadoPago\SDK::setAccessToken("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
 
 switch($_POST["type"]) {
