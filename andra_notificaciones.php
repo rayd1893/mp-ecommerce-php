3a777de252ef472edba6585d8cf7a8e5758316ad
rayd1893 <rayd1893@gmail.com>
2021-02-16 22:49:41 -0500

    Update andra_notificaciones.php

--- andra_notificaciones.php
+++ andra_notificaciones.php
@@ -8,6 +8,6 @@ $access = 'APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-6774
     $url = $uri.$id_pago.'?access_token='.$access;
 	print $url;
 	echo file_get_contents($url);
-}
+
 
     ?>
