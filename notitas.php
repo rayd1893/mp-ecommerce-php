b7da46e3641b0923246806f744eb793c3f19ee50
rayd1893 <rayd1893@gmail.com>
2021-02-16 22:12:24 -0500

    Update notitas.php

--- notitas.php
+++ notitas.php
@@ -1,11 +1,11 @@
  <?php
 
-require __DIR__ .  '/vendor/autoload.php';
+//require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
-MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004");
+/*MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004");
 MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
-
+*/
 $datos = array(
     "type" => $_POST["type"],
     "date_created" => $_POST["date_created"]
@@ -13,12 +13,12 @@ $datos = array(
 
 file_put_contents(
     'registro.txt',
-    json_encode($_POST).PHP_EOL,
+    json_encode($datos).PHP_EOL,
     FILE_APPEND
   );
 
 
-    switch($_POST["type"]) {
+    /*switch($_POST["type"]) {
         case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
             file_put_contents(
@@ -36,6 +36,6 @@ file_put_contents(
         case "invoice":
             $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
             break;
-    }
+    }*/
 
 ?>
